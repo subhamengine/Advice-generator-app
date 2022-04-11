919e25f38fb7ee2fda16b994048f0584719bbb36
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2022-04-12 00:45:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var id = document.querySelector('.header');
+var adv = document.querySelector('.advice');
+var dice = document.querySelector('.dice');
+dice.addEventListener('click',()=>{
+    fetch('https://api.adviceslip.com/advice')
+    .then((Response=>{
+        return Response.json();
+    }))
+    .then((data=>{
+        id.innerHTML = 'ADVICE #117'+data.slip.id;
+        adv.innerHTML = data.slip.advice;
+    }))
+})
+
+
